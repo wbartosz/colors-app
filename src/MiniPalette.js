90323a8c1400c2c3a6ff3d70d6ca4de2e43c0e8a
wbartosz <wojcicki.bartosz94@gmail.com>
2020-07-22 00:42:14 +0100

    Finish styling MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -14,7 +14,11 @@ const styles = {
     }
   },
   colors: {
-    backgroundColor: 'grey'
+    backgroundColor: 'white',
+    height: '150px',
+    width: '100%',
+    borderRadius: '5px',
+    overflow: 'hidden'
   },
   title: {
     display: 'flex',
@@ -29,17 +33,30 @@ const styles = {
   emoji: {
     marginLeft: '0.5rem',
     fontSize: '1.5rem'
+  },
+  miniColor: {
+    height: '25%',
+    width: '20%',
+    display: 'inline-block',
+    margin: '0 auto',
+    position: 'relative',
+    marginBottom: '-4px'
   }
 }
 
 function MiniPalette(props) {
-  const { classes, paletteName, emoji } = props;
+  const { classes, paletteName, emoji, colors } = props;
+  const miniColorBoxes = colors.map(color => (
+    <div
+      className={classes.miniColor}
+      style={{ backgroundColor: color.color }}
+      key={color.name}
+    />
+  ));
 
   return (
     <div className={classes.root}>
-      <div className={classes.colors}>
-        {/* <div></div> */}
-      </div>
+      <div className={classes.colors}>{miniColorBoxes}</div>
       <h5 className={classes.title}>
         {paletteName} <span className={classes.emoji}>{emoji}</span>
       </h5>
