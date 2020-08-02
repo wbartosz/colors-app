9bf1a2ef7eba4d03fff99260cc292c77554071e6
wbartosz <wojcicki.bartosz94@gmail.com>
2020-08-02 01:27:23 +0100

    Refactor ColorBox styles

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -43,7 +43,7 @@ class SingleColorPalette extends Component {
         key={color.name}
         name={color.name}
         background={color[format]}
-        showLink={false}
+        showingFullPalette={false}
       />
     ));
 
